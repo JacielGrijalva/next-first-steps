c6a823607a9d0ca15fbc2202db3af17a427d0698
jaciel grijalva <jaciellisandro@gmail.com>
2024-06-18 16:41:12 -0700

    cambios en home

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
     return (
         <main className="flex flex-col items-center justify-between p-24">
-            <span className="text-5xl">Holii, Que onda danilo unas ultra o que?? </span>
+            <span className="text-5xl">Holii, listo para llorar sin poder hacer nada </span>
 
             <Link href={'/about'}>About Page</Link>
         </main>
